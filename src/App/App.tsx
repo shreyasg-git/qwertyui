2fd099edc09a1c036db7de09da185c6bb1343258
Shreyas Gangurde <57534229+shreyasg-git@users.noreply.github.com>
2022-02-19 21:19:19 +0530

    TODO added to Modals, video recorded.

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -2,6 +2,11 @@ import React from "react";
 import "./App.css";
 import "./SimpleCheckBox.css";
 import MultiSelectDropdown from "components/MultiSelectDropdown";
+import Modal from "components/Modal";
+import Button from "components/Button";
+
+// TODO: if you load Modal and MultiSelectDropdow, the z-index of dropdown is at the same level of the
+// Modal body. which makes it look like highlighted because opacity doesnt work.
 
 const dropdownOpts = [
   { key: "1", title: "1" },
@@ -15,32 +20,18 @@ const dropdownOpts = [
 ];
 
 function App() {
+  const [showModal, setshowModal] = React.useState(false);
   return (
     <div>
       <div>
+        <Modal show={showModal} modalCloser={() => setshowModal(false)} title="Hello Wworld">
+          <h3>Shreyas Here</h3>
+        </Modal>
+        <Button onClick={() => setshowModal(true)}> Show Modal </Button>
         <MultiSelectDropdown options={dropdownOpts} />
-      </div>
-      <div>
-        <SimpleCheckBox />
       </div>
     </div>
   );
 }
-
-const SimpleCheckBox: React.FC = () => {
-  return (
-    <div>
-      <label className="form-control">
-        <input type="checkbox" name="checkbox" />
-        Checkbox
-      </label>
-
-      <label className="form-control">
-        <input type="checkbox" name="checkbox-checked" checked />
-        Checkbox - checked
-      </label>
-    </div>
-  );
-};
 
 export default App;
---
+++ src/components/Dropdown/Dropdown.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+// https://developer.microsoft.com/en-us/fluentui#/controls/web/dropdown
+
+type DropdownProps = {};
+
+const Dropdown: React.FC<DropdownProps> = ({}) => {
+  return <div>Dropdown</div>;
+};
+
+export default Dropdown;
